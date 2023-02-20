7368bac2fa190c901777e7b4417223dd1f3cd97f
Vitorio Tuckers <vitoriofibla@hotmail.com>
2023-02-20 16:17:19 -0300

    Added HTML, Styling, JS functionality Know bug : something is wrong with the library array

---
+++ README.md
@@ -0,0 +1,13 @@
+# Odin-Etch-a-Sketch
+
+This is a solution for the [Odin-Project](https://www.theodinproject.com/) Library challenge in their Foundations course.
+
+## How to run it
+
+Open the following link on your preferred browser:
+
+[Library](https://vitoriotuckers.github.io/Odin-Library/)
+
+## Description
+
+This is a library style app where the user can input information about a given book of their choice. this is a work in progress version.
---
+++ book.js
@@ -0,0 +1,33 @@
+const libraryContainer = document.querySelector('#library');
+let library = [];
+
+export class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.displayBook();
+    this.deleteBook();
+  }
+
+  deleteBook() {
+    const bookDeleteButton = document.querySelector('.book-delete');
+    const book = document.querySelector('.book-card');
+    bookDeleteButton.addEventListener('click', e => {
+      book.remove();
+      const index = library.indexOf(this);
+      index > -1 ? library.splice(index, 1) : null;
+    });
+  }
+
+  displayBook() {
+    const book = `<article class="book-card">
+          <h1 class="book-title">${this.title}</h1>
+          <h2 class="book-author">${this.author}</h2>
+          <h3 class="book-pages">${this.pages} <span>Pages</span></h3>
+          <button class="book-delete">&#128465;</button>
+          </article>
+        `;
+    libraryContainer.insertAdjacentHTML('afterbegin', book);
+  }
+}
