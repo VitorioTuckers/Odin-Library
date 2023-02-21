bc2275a5a6c0c1063e5265191303b6d7f691afdc
Vitorio Tuckers <vitoriofibla@hotmail.com>
2023-02-21 10:23:49 -0300

    Changed page style Optimized parts of the CSS and HTML files Changed class names

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 import { Book } from './book.js';
 
-const addBookButton = document.querySelector('#add-btn');
-const formCloseButton = document.querySelector('#form-close');
+const addBookButton = document.querySelector('.add-btn');
+const formCloseButton = document.querySelector('.form-close-btn');
 
-const popup = document.querySelector('#popup');
-const form = document.querySelector('#form');
+const popup = document.querySelector('.popup');
+const form = document.querySelector('.book-form');
 
 export let library = [];
 
-function togglePopUp() {
+const togglePopUp = () => {
   popup.classList.toggle('hide');
   form.reset();
-}
+};
 
 addBookButton.addEventListener('click', togglePopUp);
 formCloseButton.addEventListener('click', togglePopUp);
